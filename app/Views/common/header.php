9cf5b48bd068397c33111a9731754f6fc58f0b7d
DaniloKy <danilokymhyr@gmail.com>
2023-06-29 16:24:42 +0100

    navbar css

--- app/Views/common/header.php
+++ app/Views/common/header.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href="http://example.com/favicon.png">
+        <link rel="icon" href="/images/postgresql-logo-24.png">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
         <link rel="stylesheet" href="/css/main.css">
         <?php if(esc(isset($cssPath))): ?>
--- app/Views/common/menu.php
+++ app/Views/common/menu.php
@@ -1,38 +1,41 @@
 <nav >
     <ul class="navbar-list">
-        <li>
-            <a href="<?=base_url('/')?>">SRV UTP</a>
+        <li class="logo">
+            <a href="<?=base_url('/')?>"><p><i class="bx bx-lg bxl-postgresql" ></i></p></a>
         </li>
         <li>
-            <a href="<?=base_url('how-to-play')?>">GAME</a>
+            <a href="<?=base_url('how-to-play')?>">Game</a>
         </li>
         <li>
-            <a href="<?=base_url('classes')?>">CLASSES</a>
+            <a href="<?=base_url('classes')?>">Classes</a>
         </li>
         <li>
-            <a href="<?=base_url('announcements/news')?>">NEWS</a>
+            <a href="<?=base_url('announcements')?>">Announcements</a>
         </li>
         <li>
-            <a href="<?=base_url('announcements/patch_notes')?>">PATCH NOTES</a>
+            <a href="<?=base_url('announcements/news')?>">News</a>
+        </li>
+        <li>
+            <a href="<?=base_url('announcements/patch_notes')?>">Patch notes</a>
         </li>
         <?php if(session('userdata') == null || !session('userdata')['logged_in']): ?>
-        <li>
-            <a href="<?=base_url('login');?>">SIGN IN</a>
-            <a href="<?=base_url('login');?>">PLAY</a>
+        <li class="last">
+            <a href="<?=base_url('login');?>">Sing in</a>
+            <a class="play_btn" href="<?=base_url('login');?>">Play</a>
         </li>
         <?php else: ?>
-        <li>
+        <li class="last">
             <?php if(session('userdata')['user']['super']):?>
                 <a href="<?=base_url('user/admin/manage');?>"><i class="bx bx-crown bx-sm"></i></a>
             <?php endif;?>
-            <span class="dropdown"><i class="bx bx-user-circle bx-sm"></i></span>
-            <div class="dropdown_content visually-hidden">
-                <a href="<?=base_url('user/manage')?>">ACCOUNT</a>
-                <a href="<?=base_url('logout')?>">LOGOUT</a>
-            </div>
-            <a href="<?=base_url('game/character/list')?>">PLAY</a>
+            <span class="dropdown"><i class="bx bx-user-circle bx-sm"></i></span>        
+            <a class="play_btn" href="<?=base_url('game/character/list')?>">PLAY</a>
         </li>
         <?php endif; ?>
     </ul>
+    <div class="dropdown_content visually-hidden">
+        <a href="<?=base_url('user/manage')?>"><p>Account</p></a>
+        <a href="<?=base_url('logout')?>"><p>Logout</p></a>
+    </div>
 </nav>
 <main>
