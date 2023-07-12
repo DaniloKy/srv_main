1cc706991c64da31830fdeb2e2d657b237d18e81
DaniloKy <danilokymhyr@gmail.com>
2023-07-12 19:29:42 +0100

    footer

--- app/Views/common/footer.php
+++ app/Views/common/footer.php
@@ -1,6 +1,21 @@
         </main>
         <footer>
-            <p>Footer</p>
+            <div>
+                <p>&copy; 2023 Survive Utopia. All rights reserved.</p>
+            </div>
+            <div>
+                <p><strong>Made by:</strong><a href="https://github.com/DaniloKy">Danilo Kymhyr</a></p>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <p><i class='bx bx-phone'></i><strong>Phone: </strong>+351 967 260 020</p>
+                    </li>
+                    <li>
+                        <p><i class='bx bx-envelope' ></i><strong>MAIL: </strong>danilokymhyr3@gmail.com</p>
+                    </li>
+                </ul>
+            </div>
         </footer>
     </body>
 </html>
